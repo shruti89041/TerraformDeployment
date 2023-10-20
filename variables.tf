8a0dd08008371505ca28a878079a6154e39ae707
shruti89041 <85683876+shruti89041@users.noreply.github.com>
2023-10-20 16:48:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,23 @@ variable "env_alias" {
   type        = string
   default     = "v4"
 }
+
+variable "nexus_url" {
+  description = "The URL of the file in Nexus."
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
+}
+
+variable "s3_object_prefix" {
+  description = "The object key prefix in the S3 bucket."
+  type        = string
+}
+
+variable "local_download_path" {
+  description = "Local path to save the downloaded file."
+  type        = string
+}
