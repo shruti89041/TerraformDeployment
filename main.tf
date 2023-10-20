52050163f8bd2218f156c19d21f9a2ed4ffbe3a3
shruti89041 <85683876+shruti89041@users.noreply.github.com>
2023-10-20 10:52:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,8 @@
-
-
-resource "aws_kinesis_stream" "example_stream" {
-  name             = "example-stream"
-  shard_count      = 1
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_lambda_function" "example_lambda" {
-  function_name = "example-lambda"
-  handler      = "index.handler"
-  runtime      = "nodejs14.x"
-
-  role = aws_iam_role.lambda_exec_role.arn
-
-  environment {
-    variables = {
-      KINESIS_STREAM_NAME = aws_kinesis_stream.example_stream.name
-    }
-  }
-
-  depends_on = [aws_kinesis_stream.example_stream]
-
-  filename = "lambda.zip"  # Specify the path to your deployment package (lambda.zip)
-}
-
+# Step 1: IAM Role Section
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda-exec-role"
 
@@ -38,4 +18,56 @@ resource "aws_iam_role" "lambda_exec_role" {
       }
     ]
   })
+
+  # Attach inline policies or managed policies here as needed
+  # ...
 }
+
+# Step 2: AWS Lambda Section
+resource "aws_lambda_function" "example_lambda" {
+  function_name = "example-lambda"
+  handler      = "index.handler"
+  runtime      = "nodejs14.x"
+
+  role = aws_iam_role.lambda_exec_role.arn
+
+  # Define Lambda function's deployment package, environment, and other configurations
+  # ...
+
+  depends_on = [aws_iam_role.lambda_exec_role]
+}
+
+# Step 3: Nexus to S3 Data Transfer Modules
+module "nexus_s3_upload" {
+  source = "./nexus_s3_upload_module"
+
+  aws_region       = "us-east-1"
+  nexus_url        = "https://your-nexus-url/your-file.zip"
+  s3_bucket        = "your-s3-bucket-name"
+  s3_object_prefix = "your/object/key/prefix/file.zip"
+}
+
+module "nexus_s3_download" {
+  source = "./nexus_s3_download_module"
+
+  # Define input variables for the download module as needed
+  # ...
+}
+
+# Step 4: AWS Kinesis Section
+resource "aws_kinesis_stream" "example_stream" {
+  name        = "example-stream"
+  shard_count = 1
+
+  # Configure the Kinesis stream as needed
+  # ...
+}
+
+output "s3_bucket_name" {
+  value = module.nexus_s3_upload.s3_bucket_name
+}
+
+output "s3_object_key" {
+  value = module.nexus_s3_upload.s3_object_key
+}
+
