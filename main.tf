eafce0e3b4c7ab0c1500d271c7a08143bfb69087
shruti89041 <85683876+shruti89041@users.noreply.github.com>
2023-10-20 16:41:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 # Step 1: IAM Role Section
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda-exec-role"
@@ -20,6 +19,37 @@ resource "aws_iam_role" "lambda_exec_role" {
   # ...
 }
 
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "your-bucket-name"
+  acl    = "public-read-write" # By default, set the ACL to private
+
+  # Additional bucket configurations as needed
+}
+
+# Step 2: Define an S3 Bucket Policy
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicRead",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+                    "s3:PutObject"
+        Resource  = aws_s3_bucket.example_bucket.arn
+        Condition = {
+          StringEquals = {
+            "s3:ExistingObjectTag/allow-public-read" = "true"
+          }
+        }
+      }
+    ]
+  })
+}
 # Step 2: AWS Lambda Section
 resource "aws_lambda_function" "example_lambda" {
   function_name = "example-lambda"
@@ -36,11 +66,11 @@ resource "aws_lambda_function" "example_lambda" {
 
 # Step 3: Nexus to S3 Download Module
 module "nexus_s3_download" {
-  source = "./nexus_s3_download_module"
+  source = "./nexus_s3_download_module" # Replace with the actual module source
 
   aws_region   = "us-east-1"
-  nexus_url    = "https://your-nexus-url/your-file.zip"
-  local_path   = "/path/to/local/downloaded/file.zip"
+  nexus_url    = "https://your-nexus-url/your-file.zip" # URL of the file in Nexus
+  local_path   = "/path/to/local/downloaded/file.zip" //s3 folder
 }
 
 # Step 4: Nexus to S3 Upload Module
@@ -48,8 +78,8 @@ module "nexus_s3_upload" {
   source = "./nexus_s3_upload_module"
 
   aws_region       = "us-east-1"
-  s3_bucket        = "your-s3-bucket-name"
-  s3_object_prefix = "your/object/key/prefix/file.zip"
+  s3_bucket        = "your-s3-bucket-name"  # S3 bucket name
+  s3_object_prefix = "s3://my-bucket/my-folder/my-file.txt" # Object key prefix in S3
   local_path       = module.nexus_s3_download.local_path
 }
 
@@ -60,12 +90,4 @@ resource "aws_kinesis_stream" "example_stream" {
 
   # Configure the Kinesis stream as needed
   # ...
-}
-
-output "s3_bucket_name" {
-  value = module.nexus_s3_upload.s3_bucket_name
-}
-
-output "s3_object_key" {
-  value = module.nexus_s3_upload.s3_object_key
 }
