b371f53ead61eaa94193fecee3d2025d8d8c79c5
shruti89041 <85683876+shruti89041@users.noreply.github.com>
2023-10-21 22:52:41 +0530

    Update main.tf

--- Lambda-module/main.tf
+++ Lambda-module/main.tf
@@ -1,1 +1,22 @@
+resource "aws_lambda_function" "example_lambda" {
+  function_name = var.function_name
+  handler      = var.handler
+  runtime      = var.runtime
+  memory_size  = var.memory_size
+  timeout      = var.timeout
+
+  role    = var.role
+  layers  = var.layers
+
+  environment {
+    variables = var.environment
+  }
+
+  # ... Additional AWS Lambda configuration ...
+
+  // Output the Lambda function's ARN
+  output "lambda_function_arn" {
+    value = aws_lambda_function.example_lambda.arn
+  }
+}
 
