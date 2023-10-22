8a7442b54a5f1f9e69a17df6ff872b9962f74db0
shruti89041 <85683876+shruti89041@users.noreply.github.com>
2023-10-22 12:05:06 +0530

    Update main.tf

--- Lambda-module/main.tf
+++ Lambda-module/main.tf
@@ -1,3 +1,30 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-exec-role"
+
+  # Define the assume_role_policy and other configurations for the IAM role here.
+}
+
+# Rest of your resources (e.g., Lambda function) go here
+
+resource "aws_kinesis_stream" "kinesis_stream" {
+  name             = var.stream_name
+  shard_count      = var.shard_count
+  retention_period = 48
+
+  stream_mode_details {
+    stream_mode = "PROVISIONED"
+  }
+}
+
+resource "aws_s3_bucket_object" "nexus_to_s3_upload" {
+  bucket = var.bucket_name
+  key    = var.object_key
+  source = var.source_url
+
+  # ACL can be configured according to your requirements
+  # acl = "private"
+}
+
 resource "aws_lambda_function" "example_lambda" {
   function_name = var.function_name
   handler      = var.handler
@@ -5,18 +32,18 @@ resource "aws_lambda_function" "example_lambda" {
   memory_size  = var.memory_size
   timeout      = var.timeout
 
-  role    = var.role
+  role    = aws_iam_role.lambda_exec_role.arn
   layers  = var.layers
 
   environment {
     variables = var.environment
   }
 
-  # ... Additional AWS Lambda configuration ...
-
-  // Output the Lambda function's ARN
-  output "lambda_function_arn" {
-    value = aws_lambda_function.example_lambda.arn
-  }
+resource "aws_lambda_event_source_mapping" "kinesis_trigger" {
+  event_source_arn  = aws_kinesis_stream.example_stream.arn
+  function_name     = aws_lambda_function.example_lambda.function_name
+  enabled           = true
+  batch_size        = 100
+  starting_position = "LATEST"
 }
 
